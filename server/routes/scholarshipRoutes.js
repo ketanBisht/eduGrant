f1c3d271002b69cb9149815441be0df3a3c84fa7
dikshantkaushik <dikshukaushik18@gmail.com>
2026-03-18 16:25:34 +0530

    Protect scholarship routes with isAdmin and isAuthenticated

--- server/routes/scholarshipRoutes.js
+++ server/routes/scholarshipRoutes.js
@@ -6,16 +6,17 @@ import {
   updateScholarship,
   deleteScholarship
 } from "../controllers/scholarshipController.js";
+import { isAuthenticated, isAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/")
   .get(getAllScholarships)
-  .post(createScholarship);
+  .post(isAuthenticated, isAdmin, createScholarship);
 
 router.route("/:id")
   .get(getScholarshipById)
-  .put(updateScholarship)
-  .delete(deleteScholarship);
+  .put(isAuthenticated, isAdmin, updateScholarship)
+  .delete(isAuthenticated, isAdmin, deleteScholarship);
 
 export default router;
